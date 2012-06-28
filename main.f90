3ad36324520052cd4b2d76a5c3d9de48e36322cc
vitroid <vitroid@gmail.com>
2012-06-28 16:12:58 +0900

    Made it more descriptive.

--- main.f90
+++ main.f90
@@ -3,11 +3,12 @@ program clustermd
   !molecular properties
   !Increase number of molecules
   integer      :: num_molecule
-  real(kind=8) :: x(8),y(8),z(8)
-  real(kind=8) :: vx(8),vy(8),vz(8)
-  real(kind=8) :: ax(8),ay(8),az(8)
-  real(kind=8) :: fx(8),fy(8),fz(8)
-  real(kind=8) :: mass
+  real(kind=8) :: position(3,8)
+  real(kind=8) :: velocity(3,8)
+  real(kind=8) :: accel(3,8)
+  real(kind=8) :: force(3,8)
+  real(kind=8) :: argon_mass
+  real(kind=8) :: argon_eps,argon_sig
   real(kind=8) :: dt
   real(kind=8) :: k
 
@@ -16,85 +17,56 @@ program clustermd
   integer :: i,j
   integer :: ix,iy,iz
   integer :: j1,j2
-  real(kind=8) :: dx,dy,dz,dd
+  real(kind=8) :: delta(3),dd
   real(kind=8) :: ep,ek
-  real(kind=8) :: eps,sig
   i = 0
   !Place molecules on the lattice.
   do ix = 1,2
      do iy = 1,2
         do iz = 1,2
            i = i + 1
-           x(i) = ix * 4.0d0
-           y(i) = iy * 4.0d0
-           z(i) = iz * 4.0d0
+           position(1,i) = ix * 4.0d0
+           position(2,i) = iy * 4.0d0
+           position(3,i) = iz * 4.0d0
         enddo
      enddo
   enddo
   num_molecule = i
-  do i=1,num_molecule
-     vx(i) = 0.0d0
-     vy(i) = 0.0d0
-     vz(i) = 0.0d0
-     ax(i) = 0.0d0
-     ay(i) = 0.0d0
-     az(i) = 0.0d0
-     !force will be reset each step.
-  enddo
+  velocity(:,1:num_molecule) = 0.0d0
+  accel(:,1:num_molecule) = 0.0d0
   dt = 0.001
+
   !realistic interaction parameters for Argon.
-  mass = 39.95d0
-  eps = 120d0 * 0.008314d0
-  sig = 3.41d0
+  argon_mass = 39.95d0
+  argon_eps = 120d0 * 0.008314d0
+  argon_sig = 3.41d0
   num_loop = 100000
   do i=1,num_loop
      !calculate force
      !force will be reset each step.
-     do j=1,num_molecule
-        fx(j) = 0.0d0
-        fy(j) = 0.0d0
-        fz(j) = 0.0d0
-     enddo
+     force(:,1:num_molecule) = 0.0d0
      ep = 0.0
      !interaction between all pairs of molecules
      do j1 = 1,num_molecule
         do j2 = j1+1, num_molecule
-           dx = x(j2) - x(j1)
-           dy = y(j2) - y(j1)
-           dz = z(j2) - z(j1)
-           dd = dx**2 + dy**2 + dz**2
-           ep = ep + 4*eps*(sig**12/dd**6 - sig**6/dd**3)
-           k  =    - 4*eps*(12*sig**12/dd**7 - 6*sig**6/dd**4)
-           fx(j1) = fx(j1) + k * dx
-           fy(j1) = fy(j1) + k * dy
-           fz(j1) = fz(j1) + k * dz
-           fx(j2) = fx(j2) - k * dx
-           fy(j2) = fy(j2) - k * dy
-           fz(j2) = fz(j2) - k * dz
+           delta(:) = position(:,j2) - position(:,j1)
+           dd = delta(1)**2 + delta(2)**2 + delta(3)**2
+           ep = ep + 4*argon_eps*(argon_sig**12/dd**6 - argon_sig**6/dd**3)
+           k  =    - 4*argon_eps*(12*argon_sig**12/dd**7 - 6*argon_sig**6/dd**4)
+           force(:,j1) = force(:,j1) + k * delta(:)
+           force(:,j2) = force(:,j2) - k * delta(:)
         enddo
      enddo
      !calculate accel
-     do j=1,num_molecule
-        ax(j) = fx(j) / mass
-        ay(j) = fy(j) / mass
-        az(j) = fz(j) / mass
-     enddo
+     accel(:,1:num_molecule) = force(:,1:num_molecule) / argon_mass
      !calculate velocity
-     do j=1,num_molecule
-        vx(j) = vx(j) + ax(j)*dt
-        vy(j) = vy(j) + ay(j)*dt
-        vz(j) = vz(j) + az(j)*dt
-     enddo
+     velocity(:,1:num_molecule) = velocity(:,1:num_molecule) + accel(:,1:num_molecule)*dt
      !calculate position
-     do j=1,num_molecule
-        x(j) = x(j) + vx(j)*dt
-        y(j) = y(j) + vy(j)*dt
-        z(j) = z(j) + vz(j)*dt
-     enddo
+     position(:,1:num_molecule) = position(:,1:num_molecule) + velocity(:,1:num_molecule)*dt
      !calculate energies
      ek = 0.0
      do j=1,num_molecule
-        ek = ek + mass * (vx(j)**2 + vy(j)**2 + vz(j)**2) / 2.0
+        ek = ek + argon_mass * (velocity(1,j)**2 + velocity(2,j)**2 + velocity(3,j)**2) / 2.0
      enddo
      write(6,*) i,ep,ek,ep+ek
   enddo
